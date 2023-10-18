3e012cc6bb75ea9558e6139124e366c08369222a
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-10-18 22:30:51 +0000

    [App.js ]

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let tipe = ['.com', '.net', '.us', '.io'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/*
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let t = 0; t < tipe.length; t++) {
+      let direcciones = pronoun[i] + adj[j] + noun[k] + tipe[t];
+      console.log(direcciones);
+      }
+    }
+  }
+} funcionan las dos pero la segunda es mas corta*/
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+for(let items in pronoun){
+  for(let obj in adj){
+    for(let item in noun){
+      for(let obs in tipe){
+        console.log(pronoun[items]+adj[obj]+noun[item]+tipe[obs])
+      }
+    }
+  }
+}
+
