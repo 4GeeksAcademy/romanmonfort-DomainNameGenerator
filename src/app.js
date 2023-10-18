7d08212e4143b126f956d4ca441df67d2ec5b1b8
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-10-18 23:54:14 +0000

    [mejora]

--- src/app.js
+++ src/app.js
@@ -1,28 +1,41 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let tipe = ['.com', '.net', '.us', '.io'];
 
-/*
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let t = 0; t < tipe.length; t++) {
-      let direcciones = pronoun[i] + adj[j] + noun[k] + tipe[t];
-      console.log(direcciones);
+let pronouns = ['the', 'our'];
+let adjetives = ['great', 'big'];
+let nouns = ['jogger', 'racoon','internet', 'lastofus','puedes'];
+let extensions =['.com','.net','.io','.us', '.es'];
+const specialDomains = {
+  "internet": ".net",
+  "puedes": ".es",
+  "lastofus": ".us"
+};
+function generateDomainNames (){
+  let domainNames = [];
+  for(let pronoun of pronouns){
+    for(let adjetive of adjetives){
+      for(let noun of nouns){
+        for(let extension of extensions){
+          let newDomain = "";
+          // console.log(pronoun[items]+adj[obj]+noun[item]+type[obs])
+          if (noun in specialDomains && specialDomains [noun] === extension){
+            // domainNames.push (`${noun.slice(0,-extension.length + 1)}${extension}`)
+            newDomain = `${noun.slice(0,-extension.length + 1)}${extension}`;
+          } else {
+            // domainNames.push (`${pronoun}${adjetive}${noun}${extension}`)
+            newDomain = `${pronoun}${adjetive}${noun}${extension}`;
+          }
+          if (!domainNames.includes (newDomain)){
+            domainNames.push(newDomain);
+          }
+        }
       }
     }
   }
-} funcionan las dos pero la segunda es mas corta*/
-
-
-for(let items in pronoun){
-  for(let obj in adj){
-    for(let item in noun){
-      for(let obs in tipe){
-        console.log(pronoun[items]+adj[obj]+noun[item]+tipe[obs])
-      }
-    }
+  return domainNames;
+}
+function printDomainNames (domainNames){
+  for (const domain of domainNames) {
+   console.log(domain);
   }
 }
-
+const domainNames = generateDomainNames();
+printDomainNames(domainNames);
